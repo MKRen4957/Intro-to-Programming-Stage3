f3437f34664801655336bcd454d9eb72085c6f7c
Chudi Zhong <Chudi@Chudis-MacBook-Pro-2.local>
2017-03-08 19:40:57 -0600

    Add more movies

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,17 +1,26 @@
-# Lesson 3.4: Make Classes
-# Mini-Project: Movies Website
-
-# In this file, you will define instances of the class Movie defined
-# in media.py. After you follow along with Kunal, make some instances
-# of your own!
-
-# After you run this code, open the file fresh_tomatoes.html to
-# see your webpage!
-
 import media
 import fresh_tomatoes
 
-the_big_short = media.Movie("The Big Short", "WS",  "https://upload.wikimedia.org/wikipedia/en/e/e3/The_Big_Short_teaser_poster.jpg", "https://youtu.be/O1X0KDdS5Bw")
+the_big_short = media.Movie("The Big Short",  \
+"https://upload.wikimedia.org/wikipedia/en/e/e3/The_Big_Short_teaser_poster.jpg", "https://youtu.be/O1X0KDdS5Bw")
+margin_call = media.Movie("Margin Call", "https://upload.wikimedia.org/wikipedia/en/6/62/Margin_Call.jpg", \
+"https://youtu.be/Y2DqFRsPrns")
+the_wolf_of_wall_street = media.Movie("The Wolf of Wall Street", \
+"https://upload.wikimedia.org/wikipedia/en/1/1f/WallStreet2013poster.jpg", \
+"https://youtu.be/iszwuX1AK6A")
+pacific_rim = media.Movie("Pacific Rim", \
+"https://upload.wikimedia.org/wikipedia/en/f/f3/Pacific_Rim_FilmPoster.jpeg", "https://youtu.be/5guMumPFBag")
+godzilla = media.Movie("Godzilla", "https://upload.wikimedia.org/wikipedia/en/1/10/Godzilla_%282014%29_poster.jpg", \
+"https://youtu.be/vIu85WQTPRc")
+world_war_z = media.Movie("World War Z", "https://upload.wikimedia.org/wikipedia/en/d/dc/World_War_Z_poster.jpg", \
+"https://youtu.be/HcwTxRuq-uk")
+gravity = media.Movie("Gravity", "https://upload.wikimedia.org/wikipedia/en/f/f6/Gravity_Poster.jpg", \
+"https://youtu.be/OiTiKOy59o4")
+rush = media.Movie("Rush", "https://upload.wikimedia.org/wikipedia/en/d/d0/Rush_UK_poster.jpeg", \
+"https://youtu.be/4XA73ni9eVs")
+black_hawk_down = media.Movie("Black Hawk Down", \
+"https://upload.wikimedia.org/wikipedia/en/0/0c/Black_hawk_down_ver1.jpg", "https://youtu.be/QjouwbniJSs")
 
-movies = [the_big_short]
+movies = [the_big_short, margin_call, the_wolf_of_wall_street, pacific_rim, godzilla, \
+world_war_z, gravity, rush, black_hawk_down]
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,23 +1,14 @@
-# Lesson 3.4: Make Classes
-# Mini-Project: Movies Website
-
-# In this file, you will define the class Movie. You could do this
-# directly in entertainment_center.py but many developers keep their
-# class definitions separate from the rest of their code. This also
-# gives you practice importing Python files.
-
 import webbrowser
 
 class Movie():
     #This class provides a way to store movie related information
 
-    def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
         #initialize instance of class Movie
         self.title = title
-        self.storyline = storyline
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
 
-    def play_trailer(self):
+    #def play_trailer(self):
         #play the movie trailer
-        webbrower.open(self.trailer_youtube_url)
+        #webbrower.open(self.trailer_youtube_url)
